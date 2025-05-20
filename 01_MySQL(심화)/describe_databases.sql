e8606412438e9ea2376be71a82c9c3d4b5c1b720
woojeans7 <thddnwls71@naver.com>
2025-05-20 11:39:43 +0900

    Solve : 01 심화문제 풀이 05-19

---
+++ 01_MySQL(심화)/describe_databases.sql
@@ -0,0 +1,16 @@
+use world;
+show tables;
+show columns from city;
+show columns from country;
+show columns from countrylanguage;
+
+use employees;
+show tables;
+show columns from current_dept_emp;
+show columns from departments;
+show columns from dept_emp;
+show columns from dept_emp_latest_date;
+show columns from dept_manager;
+show columns from employees;
+show columns from salaries;
+show columns from titles;
